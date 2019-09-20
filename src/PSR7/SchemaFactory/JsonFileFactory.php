e8b830ec6aef2f5cb11c4ce0679dfb9b30e60485
Krishnaprasad MG <sunspikes@gmail.com>
2019-09-20 13:55:26 +0200

    Update JsonFileFactory.php

--- src/PSR7/SchemaFactory/JsonFileFactory.php
+++ src/PSR7/SchemaFactory/JsonFileFactory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace OpenAPIValidation\PSR7\SchemaFactory;
 
 use cebe\openapi\Reader;
-use cebe\openapi\ReferenceContext;
 use cebe\openapi\spec\OpenApi;
 use function realpath;
 
